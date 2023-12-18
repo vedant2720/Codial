eefcab435ed82a7e20ae5e7a4c4f0b7ab5bf5f61
Vedant Sandip Dudhale <vedantdudhale@gmail.com>
2023-12-18 22:34:33 +0530

    adding router and controller code

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+app.use('/',require('./routes'));
+
 app.listen(port,function(err){
     if(err)
     {
         console.log(`Error: ${err}`);
     }
 
+    //adding comment.
     console.log(`Server Running on ${port}`);
 });
